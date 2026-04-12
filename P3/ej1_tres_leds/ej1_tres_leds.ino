29eef2a92125e637a362f8c685251c5d35df7bb5
Darío Ortega Leyva <130694483+Darioortegaleyva@users.noreply.github.com>
2026-04-12 12:45:42 +0200

    Modificación de comentarios

--- P3/ej1_tres_leds/ej1_tres_leds.ino
+++ P3/ej1_tres_leds/ej1_tres_leds.ino
@@ -8,14 +8,14 @@ void setup(){
 
 void loop(){
   digitalWrite(13, HIGH);
-  delay(1500); // Wait for 1000 millisecond(s)
+  delay(1500); // Wait for 1500 millisecond(s)
   digitalWrite(13, LOW);
   
   digitalWrite(12, HIGH);
-  delay(1500); // Wait for 1000 millisecond(s)
+  delay(1500); // Wait for 1500 millisecond(s)
   digitalWrite(12, LOW);
   
   digitalWrite(11, HIGH);
-  delay(1500); // Wait for 1000 millisecond(s)
+  delay(1500); // Wait for 1500 millisecond(s)
   digitalWrite(11, LOW);
 }
